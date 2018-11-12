7a28de4260dba2050d7e076f199d35b9f036a4d3
GarbiSebastian <garbyseba@gmail.com>
2018-11-12 11:13:31 -0300

    Comit inicial Signed-off-by:GarbiSebastian <garbyseba@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: sebastian
+ *
+ * Created on 12 de noviembre de 2018, 09:46
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
