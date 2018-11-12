becf9174881d10b1930caf1c4a68d6dd639a8259
GarbiSebastian <garbyseba@gmail.com>
2018-11-12 15:01:34 -0300

    ignore csv Signed-off-by:GarbiSebastian <garbyseba@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,14 +6,39 @@
  */
 
 #include <cstdlib>
+#include "src/vector_builder.h"
 
 using namespace std;
+
+int mainCatedra(int argc, char** argv) {
+    auto filter_out = [] (const int token, const FrecuencyVocabularyMap & vocabulary) {
+        /**
+         *  Lambda para usar como filtro de vocabulario
+         *  Retorna `true` si `token` debe eliminarse
+         *  Retorna `false` si `token` no debe eliminarse
+         **/
+        double token_frecuency = vocabulary.at(token);
+        return token_frecuency < 0.1 || token_frecuency > 0.5;
+    };
+    std::string entries_path = "datos/imdb_tokenized.csv";
+    VectorizedEntriesMap train_entries;
+    VectorizedEntriesMap test_entries;
+    build_vectorized_datasets(entries_path, train_entries, test_entries, filter_out);
+    int N = train_entries.begin()->second.bag_of_words.size();
+
+    std::cerr
+            << "Tamaño de los bags of words: " << N << " tokens" << std::endl
+            << "Tamaño del dataset de training: " << train_entries.size() << " entradas" << std::endl
+            << "Tamaño del dataset de testing: " << test_entries.size() << " entradas" << std::endl;
+
+    return 0;
+}
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-
-    return 0;
+    return mainCatedra(argc, argv);
+    //    return 0;
 }
 
