3d5e07e753b2f1a86ccd641d74091e7a947d2d17
GarbiSebastian <garbyseba@gmail.com>
2018-11-21 17:15:18 -0300

    ejecutar alla

--- herramientas/ejecutarFoldsPca.php
+++ herramientas/ejecutarFoldsPca.php
@@ -12,10 +12,9 @@
 //const string paramK                 = "-k";
 //const string paramAlfa              = "-a";
 
-$folds = [2, 5, 10, 25];
-$folds = [25, 10];
+$folds = [25, 10, 5, 2];
 //$ks = [1, 3, 5, 11, 31, 41, 49, 51, 61, 71, 91, 101];
-$ks = [41];
+$ks = [31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 1, 3, 5, 11, 61, 71, 91, 101];
 $alfas = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 $metodo = 1;
 
@@ -23,19 +22,21 @@ foreach ($folds as $fold) {
     foreach ($ks as $k) {
         foreach ($alfas as $alfa) {
             foreach (range(1, $fold) as $iteracion) {
-                $dirResultado = "resultados/metodo-$metodo/knn-$k/alfa-$alfa/$fold-fold";
-                exec("mkdir -p $dirResultado");
-                $comandoKnn = "./tp"
-                        . " -m $metodo"
-                        . " -d datos/fold/$fold-fold/iter-$iteracion.csv"
-                        . " -o $dirResultado/clasif-$iteracion.csv"
-                        . " -r $dirResultado/real-$iteracion.csv"
-                        . " -k $k"
-                        . " -a $alfa"
-                        . " -x $dirResultado/extra-$iteracion.txt"
-                        . " 2> /dev/null";
-                echo $comandoKnn . PHP_EOL;
-                exec($comandoKnn);
+                if (!file_exists("$dirResultado/clasif-$iteracion.csv")) {
+                    $dirResultado = "resultados/metodo-$metodo/knn-$k/alfa-$alfa/$fold-fold";
+                    exec("mkdir -p $dirResultado");
+                    $comandoKnn = "./tp"
+                            . " -m $metodo"
+                            . " -d datos/fold/$fold-fold/iter-$iteracion.csv"
+                            . " -o $dirResultado/clasif-$iteracion.csv"
+                            . " -r $dirResultado/real-$iteracion.csv"
+                            . " -k $k"
+                            . " -a $alfa"
+                            . " -x $dirResultado/extra-$iteracion.txt"
+                            . " 2> /dev/null";
+                    echo $comandoKnn . PHP_EOL;
+                    exec($comandoKnn);
+                }
             }
         }
     }
